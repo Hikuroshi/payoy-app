52729e33579132a08ca246a62004ff8e4d9ba53f
mariakalya27-dev <mariakalya27@gmail.com>
2026-05-08 22:57:56 +0700

    add error page content

--- app/(site)/error.tsx
+++ app/(site)/error.tsx
@@ -11,14 +11,12 @@ export default function DashboardError({ error, reset }: { error: Error & { dige
   return (
     <div className="flex min-h-[60vh] items-center justify-center px-4">
       <div className="flex flex-col items-center text-center max-w-sm gap-4">
-        {/* ICON */}
         <img src="/img/error page 2.svg" alt="Error Illustration" className="w-80 h-80 object-contain w-[420px] object-contain" />
 
-        {/* TITLE */}
         <h1 className="text-xl font-semibold">Halaman gagal dimuat</h1>
-        {/* DESCRIPTION */}
+
         <p className="text-sm text-muted-foreground mb-3">Kami tidak dapat menampilkan isi halaman saat ini. Silakan muat halaman kembali.</p>
-        {/* BUTTON */}
+
         <Button onClick={() => reset()} className="bg-orange-500 hover:bg-orange-600 mb-10">
           Muat Halaman
         </Button>
--- app/table/[id]/error.tsx
+++ app/table/[id]/error.tsx
@@ -11,14 +11,12 @@ export default function DashboardError({ error, reset }: { error: Error & { dige
   return (
     <div className="flex min-h-[60vh] items-center justify-center px-4">
       <div className="flex flex-col items-center text-center max-w-sm gap-4">
-        {/* ICON */}
         <img src="/img/error page 2.svg" alt="Error Illustration" className="w-80 h-80 object-contain w-[420px] object-contain mb-[-60 px]" />
 
-        {/* TITLE */}
         <h1 className="text-xl font-semibold">Proses pemesanan gagal dimuat</h1>
-        {/* DESCRIPTION */}
+
         <p className="text-sm text-muted-foreground mb-3">Kami tidak dapat meneruskan proses pemesanan saat ini. Silakan muat halaman kembali.</p>
-        {/* BUTTON */}
+
         <Button onClick={() => reset()} className="bg-orange-500 hover:bg-orange-600">
           Muat Halaman
         </Button>
